2b98b1d19a8250b2b488f1c6c1dd1e67363d80a2
CodeOmari <jacksonomari2230@gmail.com>
2025-01-11 07:33:40 +0300

    Python Functions and Data Structures

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -20,8 +20,8 @@ def main():
             print(f"{temp}°C is {result:.2f}°F")
         else:
             print("Invalid input. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
-    except ValueError as e:
-        print(e)
+    except ValueError:
+        raise ValueError("Invalid temperature. Please enter a numeric value.")
 
 if __name__ == "__main__":
     main()
