56af25e0819bfb2eaed03bda2fa3f4d5b70e8716
CodeOmari <jacksonomari2230@gmail.com>
2025-01-17 12:55:00 +0300

    Python OOP

---
+++ programming-paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        if float(amount) > 0:
+            self.account_balance += amount
+        else:
+            return f"Unable to deposit {amount}"
+        
+    def withdraw(self, amount):
+        if 0 < self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
+
+    def display_balance(self):
+        print(f"Current Balance: ${float(self.account_balance):.2f}")
